051499fb89a5ba843030cdb621ce2c058526a338
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2022-03-30 19:09:54 +0200

    update slider style

--- ESP32-ZeppelinCAM/zeppelincam_html.h
+++ ESP32-ZeppelinCAM/zeppelincam_html.h
@@ -71,16 +71,12 @@ figure img{
 
 </style>
 <style>
-#slidecontainer {
-  width: 100%;
-  height: 15vh;
-}
 .slider-color {
   -webkit-appearance: none;
   width: 100%;
-  height: 15px;
+  height: 20px;
   margin-top: 10px;
-  margin-bottom: 10px;  
+  margin-bottom: 15px;  
   border-radius: 5px;
   background: #d3d3d3;
   outline: none;
@@ -94,16 +90,15 @@ figure img{
 .slider-color::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
-  width: 25px;
-  height: 25px;
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
   background: #4CAF50;
   cursor: pointer;
 }
 .slider-color::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  margin: auto;
+  width: 40px;
+  height: 40px;
   border: 0;
   border-radius: 50%;
   background: #4CAF50;
