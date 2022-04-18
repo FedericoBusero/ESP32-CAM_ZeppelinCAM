94c4e631d87e3d014c4e00ac348c829fa018e4d1
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2022-04-18 21:15:45 +0200

    oneventscallback niet langer nodig

--- ESP32-ZeppelinCAM/ESP32-ZeppelinCAM.ino
+++ ESP32-ZeppelinCAM/ESP32-ZeppelinCAM.ino
@@ -417,28 +417,6 @@ void handleJoystick(int x, int y)
   updateMotors();
 }
 
-void onEventsCallback(WebsocketsEvent event, String data) {
-  if (event == WebsocketsEvent::ConnectionOpened) {
-#ifdef DEBUG_SERIAL
-    DEBUG_SERIAL.println(F("Connnection Opened"));
-#endif
-  } else if (event == WebsocketsEvent::ConnectionClosed) {
-#ifdef DEBUG_SERIAL
-    DEBUG_SERIAL.println(F("Connnection Closed"));
-#endif
-  } else if (event == WebsocketsEvent::GotPing) {
-#ifdef DEBUG_SERIAL
-    DEBUG_SERIAL.println(F("Got a Ping!"));
-#endif
-  } else if (event == WebsocketsEvent::GotPong) {
-#ifdef DEBUG_SERIAL
-    DEBUG_SERIAL.println(F("Got a Pong!"));
-#endif
-    analogwrite_channel(CHANNEL_ANALOGWRITE_LED, LED_BRIGHTNESS_HANDLEMESSAGE); 
-    last_activity_message = millis();
-  }
-}
-
 void handle_message(WebsocketsMessage msg) {
   const char *msgstr = msg.c_str();
   const char *p;
@@ -609,7 +587,6 @@ void loop()
     DEBUG_SERIAL.println(F("Connection accept"));
 #endif
     sclient.onMessage(handle_message);
-    sclient.onEvent(onEventsCallback); // run callback when events are occuring
     onConnect();
     is_connected = 1;
   }
