2707c2bf5dab183c3319b00d87d6524c5bc92b47
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2022-04-18 21:33:42 +0200

    Update zeppelincam_html.h

--- ESP32-ZeppelinCAM/zeppelincam_html.h
+++ ESP32-ZeppelinCAM/zeppelincam_html.h
@@ -1,4 +1,4 @@
-const uint8_t index_html[] PROGMEM = R"=====(
+const char index_html[] PROGMEM = R"=====(
 <html>
 <head>
 <meta name='viewport'         content='width=device-width,         initial-scale=1.0,         user-scalable=no' />
